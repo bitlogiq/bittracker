96ac51266dbed81e2387d14737c7a7d14138c2f9
Roelof.Horowitz <roeliehorowitz@gmail.com>
2025-08-24 20:55:01 +0200

    Normalize org IDs to za.co.bitlogiq.bittracker, regenerate android/ios/web, update build.gradle.kts + MainActivity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android") // modern plugin id
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.tracker"
+    namespace = "za.co.bitlogiq.bittracker"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.tracker"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "za.co.bitlogiq.bittracker"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,9 +29,16 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Using debug signing so `flutter run --release` works out of the box.
             signingConfig = signingConfigs.getByName("debug")
+            // If you have a release keystore, replace the above with your release signingConfig.
+        }
+    }
+
+    // Avoids occasional META-INF license collisions from transitive deps
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
